f31dd2294b75282881035da401a1b0ae13ed1f16
Hamoud87 <73738739+Hamoud87@users.noreply.github.com>
2021-05-30 12:49:51 +0200

    update

---
+++ js/shoppingCart.js
@@ -0,0 +1,16 @@
+const cartItems = JSON.parse(localStorage.getItem("proList"));
+const cartContainer = document.querySelector(".shoppingcart-elements");
+const totalContainer = document.querySelector(".total");
+let total = 0;
+cartItems.forEach(function (cartElement) {
+  total = cartElement.price;
+  cartContainer.innerHTML += `
+
+    <div class="pro-list">
+    <h5>${cartElement.name}</h5>
+    <img src="${cartElement.image}"  class="cart-image cart-thumbnail" "alt="${cartElement.alt}"  />
+    </div>
+   
+    `;
+});
+totalContainer.innerHTML = `Total: ${total}`;
